2909671149e4e3e46277242a5e8b42a451d36a38
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-15 17:31:18 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
 import express from "express";
 import { PORT, mongoDBUrl } from "./config.js";
 import mongoose from "mongoose";
+import { Todo } from "./models/todoModel.js";
 
 const app = express();
-
+app.use(express.json());
+// HomeRoute
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Hello " });
+});
+
+// SaveBook in Database
+app.post("/", async (req, res) => {
+  try {
+    if (!req.body.title) {
+      return res.status(400).send({ message: "Write required field" });
+    }
+    const newTodo = {
+      title: req.body.title,
+    };
+
+    const todo = await Todo.create(newTodo);
+
+    return res.status(200).send(todo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 mongoose
