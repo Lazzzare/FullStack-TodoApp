5bacfb21d8ae559734399f0cbac126d6ac9aa162
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-16 04:26:12 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,16 @@ const app = express();
 app.use(express.json());
 
 // HomeRoute
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "Hello " });
+app.get("/", async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.status(200).send(todos);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
-// SaveBook in Database
+// SaveTodo in Database
 app.post("/", async (req, res) => {
   try {
     if (!req.body.title) {
@@ -27,6 +32,22 @@ app.post("/", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+// DeleteTodo
+
+app.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Todo.findByIdAndDelete(id);
+    if (!result) {
+      return res.status(404).send({ message: `todo with id ${id} not found` });
+    }
+
+    return res.status(200).send({ message: "book deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
   }
 });
 
