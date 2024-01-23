cbbcefe421dca96717bf41098b8e5b403b1ee752
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-23 17:19:08 +0400

    Update Input.tsx

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,8 +1,38 @@
-const Input = () => {
+import { useState } from "react";
+import axios from "axios";
+import circle from "./../assets/Circle.svg";
+
+const Input = ({ darkMode }: { darkMode: boolean }) => {
+  const [input, setInput] = useState("");
+
+  const handleSaveInput = () => {
+    const newTodo = {
+      title: input,
+    };
+    axios.post("http://localhost:3001/", newTodo).catch((error) => {
+      console.log(error);
+    });
+  };
+
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="Create a new todo…" />
+    <div className="mx-auto -mt-24">
+      <form onSubmit={handleSaveInput}>
+        <div className="relative">
+          <input
+            className={`${
+              darkMode ? "bg-[#25273D]" : "bg-white"
+            } w-[327px] rounded-md py-[18px] pl-[52px]`}
+            type="text"
+            placeholder="Create a new todo…"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <img
+            className="absolute left-5 top-1/2 transform -translate-y-1/2"
+            src={circle}
+            alt=""
+          />
+        </div>
       </form>
     </div>
   );
