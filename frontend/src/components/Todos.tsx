8c8fbac0ab3fe4f53a0bb33f010763340234c321
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-24 00:25:56 +0400

    Update Todos.tsx

--- frontend/src/components/Todos.tsx
+++ frontend/src/components/Todos.tsx
@@ -1,7 +1,9 @@
 import dataTypes from "../types";
 import circle from "../assets/Circle.svg";
+import checkedCircle from "../assets/CompleteCircle.svg";
 import cross from "../assets/icon-cross.svg";
 import axios from "axios";
+import { useState } from "react";
 
 const Todos = ({
   todos,
@@ -26,20 +28,36 @@ const Todos = ({
       });
   };
 
+  const [check, setCheck] = useState(false);
+
   return (
-    <div className=" px-4 mt-4">
+    <div className="mt-4">
       {todos.map((todo) => {
         return (
           <div
             key={todo._id}
-            className={` rounded-t-lg w-[327px] mx-auto ${
+            className={`rounded-t-lg ${
               darkMode
                 ? "bg-[#25273D] text-[#C8CBE7]"
                 : "bg-white text-[#494C6B]"
             }`}
           >
             <div className="relative rounded-t-lg flex py-5">
-              <img src={circle} alt="circle" className="absolute left-4" />
+              {check ? (
+                <img
+                  src={checkedCircle}
+                  alt="checkedCircle"
+                  className="absolute left-4"
+                  onClick={() => setCheck(!check)}
+                />
+              ) : (
+                <img
+                  src={circle}
+                  alt="circle"
+                  className="absolute left-4"
+                  onClick={() => setCheck(!check)}
+                />
+              )}
               <h1 className="pl-[52px]">{todo.title}</h1>
               <img
                 onClick={() => handleDeleteTodo(todo._id)}
@@ -60,7 +78,7 @@ const Todos = ({
       })}
       {/* ItemsLeft and Clear Completed */}
       <div
-        className={`w-[327px] mx-auto flex items-center justify-between px-5 py-4 rounded-b-md ${
+        className={`flex items-center justify-between px-5 py-4 rounded-b-md ${
           darkMode ? "bg-[#25273D] text-[#C8CBE7]" : "bg-white text-[#494C6B]"
         }`}
       >
@@ -69,13 +87,15 @@ const Todos = ({
       </div>
       {/* All/Active/Completed */}
       <div
-        className={`w-[327px] mx-auto mt-4 flex items-center justify-evenly py-4 rounded-md ${
+        className={`${
+          darkMode ? "container-dark" : "container-light"
+        } container mt-4 flex items-center justify-evenly py-4 rounded-md ${
           darkMode ? "bg-[#25273D] text-[#C8CBE7]" : "bg-white text-[#494C6B]"
         }`}
       >
-        <h2>All</h2>
-        <h2>Active</h2>
-        <h2>Completed</h2>
+        <h2 className="cursor-pointer">All</h2>
+        <h2 className="cursor-pointer">Active</h2>
+        <h2 className="cursor-pointer">Completed</h2>
       </div>
     </div>
   );
