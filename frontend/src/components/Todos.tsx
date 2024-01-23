d50cfa39b76049a37c1a2e6bf36cb826ab413351
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-23 17:19:06 +0400

    Update Todos.tsx

--- frontend/src/components/Todos.tsx
+++ frontend/src/components/Todos.tsx
@@ -1,16 +1,61 @@
 import dataTypes from "../types";
+import circle from "../assets/Circle.svg";
+import cross from "../assets/icon-cross.svg";
 
-const Todos = ({ todos }: { todos: dataTypes[] }) => {
+const Todos = ({
+  todos,
+  darkMode,
+}: {
+  todos: dataTypes[];
+  darkMode: boolean;
+}) => {
   return (
-    <>
+    <div className="mx-auto mt-4">
       {todos.map((todo) => {
         return (
-          <div key={todo._id}>
-            <h1>{todo.title}</h1>
+          <div
+            key={todo._id}
+            className={`${
+              darkMode
+                ? "bg-[#25273D] text-[#C8CBE7]"
+                : "bg-white text-[#494C6B]"
+            }`}
+          >
+            <div className="relative w-[327px] flex py-5">
+              <img src={circle} alt="circle" className="absolute left-4" />
+              <h1 className="pl-[52px]">{todo.title}</h1>
+              <img src={cross} alt="cross" className="absolute right-4" />
+            </div>
+            <hr
+              className={`${
+                darkMode
+                  ? "bg-[#25273D] text-[#C8CBE7]"
+                  : "bg-white text-[#494C6B]"
+              }`}
+            />
           </div>
         );
       })}
-    </>
+      {/* ItemsLeft and Clear Completed */}
+      <div
+        className={`w-full flex items-center justify-between px-5 py-4 rounded-b-md ${
+          darkMode ? "bg-[#25273D] text-[#C8CBE7]" : "bg-white text-[#494C6B]"
+        }`}
+      >
+        <span>{todos.length} items left</span>
+        <button>Clear Completed</button>
+      </div>
+      {/* All/Active/Completed */}
+      <div
+        className={`mt-4 flex items-center justify-evenly py-4 rounded-md ${
+          darkMode ? "bg-[#25273D] text-[#C8CBE7]" : "bg-white text-[#494C6B]"
+        }`}
+      >
+        <h2>All</h2>
+        <h2>Active</h2>
+        <h2>Completed</h2>
+      </div>
+    </div>
   );
 };
 
